dd4ecf126be14b6d5a12a2bc67aae4a0c0cb2724
SaidSaPaiva <saidsapaiva@gmail.com>
2025-03-05 19:54:31 -0300

    Add files via upload

---
+++ Atividade1/atividade1_SAID.js
@@ -0,0 +1,21 @@
+
+const a = 60;
+const b = 20;
+
+let c = a + b;
+console.log(`O resultado da Operação de Soma entre ${a} e ${b} é: ${c}`);
+
+c = a - b;
+console.log(`O resultado da Operação de Subtração entre ${a} e ${b} é: ${c}`);
+
+c = a / b;
+console.log(`O resultado da Operação de Divisão entre ${a} e ${b} é: ${c}`);
+
+c = a * b;
+console.log(`O resultado da Operação de Multiplicação entre ${a} e ${b} é: ${c}`);
+
+
+
+
+
+
